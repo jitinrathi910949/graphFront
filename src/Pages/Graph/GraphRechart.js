ea1a6760de33cba2689c6ed04df5dbaf08db519b
Onion 9 <onion9@Onions-MacBook-Pro.local>
2020-05-02 12:42:53 +0530

    new changes

--- src/Pages/Graph/GraphRechart.js
+++ src/Pages/Graph/GraphRechart.js
@@ -26,11 +26,12 @@ export default function GraphRecharts(props) {
             <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <Line type="monotone" dataKey="y" stroke="#8884d8" dot={false} />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="x" minTickGap={30} orientation="top" tickFormatter={(label) => {
+                <XAxis dataKey="x" minTickGap={50} orientation="top" tickFormatter={(label) => {
 
-                    const tempDate = new Date(parseInt(label, 10));
-                    console.log('temp date is', tempDate);
-                    return (`${("0" + (tempDate.getMonth() + 1)).slice(-2)}-${tempDate.getFullYear()}`)
+                    const date = new Date(parseInt(label, 10));
+                    console.log('temp date is', date);
+                    const display = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '-' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '-' + date.getFullYear();
+                    return (display)
                 }} />
                 <YAxis tickFormatter={(label) => `${label}%`} />
                 <Tooltip content={<CustomTooltip />} />
