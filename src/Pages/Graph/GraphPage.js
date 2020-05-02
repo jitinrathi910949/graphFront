c59d524a7d0cbf6d4f018f441f49ddb586e11278
Onion 9 <onion9@Onions-MacBook-Pro.local>
2020-05-02 12:13:04 +0530

    new layout

--- src/Pages/Graph/GraphPage.js
+++ src/Pages/Graph/GraphPage.js
@@ -1,29 +1,28 @@
 import * as React from "react";
 import { withStyles } from '@material-ui/core/styles';
-import {Grid, Button, Card,
-     CardHeader, CardContent, 
-     Paper, CardActions,CircularProgress
-    } from '@material-ui/core'
+import {
+  Grid, Button, Card,
+  CardHeader, CardContent,
+   CircularProgress
+} from '@material-ui/core'
 import BackupOutlinedIcon from '@material-ui/icons/BackupOutlined'
 import styles from './GraphStyle'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import {uploadCsv, fetchAllData} from '../../store/actions/graphActions';
-import {isEmpty} from '../../utils/common';
-import Graph from './Graph';
+import { uploadCsv, fetchAllData } from '../../store/actions/graphActions';
+import { isEmpty } from '../../utils/common';
 import GraphRecharts from './GraphRechart';
 
 class GraphPage extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+  getAllData = () => {
+    this.props.fetchAllData();
+  }
 
-    }
-    getAllData =() => {
-      this.props.fetchAllData();
-    }
-
-    onUploadFile =(event) => {
-        event.preventDefault()
+  onUploadFile = (event) => {
+    event.preventDefault()
     const errs = []
     const files = Array.from(event.target.files)
     const formData = new FormData()
@@ -47,75 +46,75 @@ class GraphPage extends React.Component {
       }
     })
   }
-    
 
-    render() {
-        const {classes, isLoading, csvData } = this.props;
+  render() {
+    const { classes, isLoading, csvData } = this.props;
 
-        return(
-            <div className={classes.root}>
-            <Grid container 
-            direction="row"
-            justify="center"
-            alignItems="center"
-            
-          >
-                <Grid item xs={12}>
-                   
-            <Card>
-            <CardHeader title="NIFTY50 Draw Down Chart">  </CardHeader>
-            <CardActions style={{float:'right'}}>
-            <Button 
-            color="primary"
-            variant="contained"
-            startIcon={ <BackupOutlinedIcon />}
-          component="label">
-          {isLoading ?  <CircularProgress color="white" /> : 'Upload File'}
-          <input style={{ display: 'none' }} type="file" id='file' name="file"onChange= {this.onUploadFile}/>
-        </Button>
-        <Button color="primary"
-        variant="contained"
-        onClick={() => this.getAllData()}
+    return (
+      <div className={classes.root}>
+        <Grid container
+          direction="row"
+          justify="center"
+          alignItems="center"
+
         >
-          Get All Data
-        </Button>
-            </CardActions>
-            <CardContent style={{marginTop: '25px'}}>
-           {/* {!isEmpty(csvData) && <Graph csvData={csvData} />} */}
-           {/* <Graph csvData={csvData} /> */}
-           <GraphRecharts csvData={csvData} classes={classes} />
-            </CardContent>
-    
-            </Card>
-            
-                </Grid>
-            </Grid>
-            </div>
-        );
-    
+          <Grid item xs={12}>
 
-}
+            <Card>
+              <CardHeader title="NIFTY50 Draw Down Chart"
+                action={
+                  <>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      style={{ marginLeft: '10px' }}
+                      startIcon={<BackupOutlinedIcon />}
+                      component="label">
+                      {isLoading ? <CircularProgress color="white" /> : 'Upload File'}
+                      <input style={{ display: 'none' }} type="file" id='file' name="file" onChange={this.onUploadFile} />
+                    </Button>
+                    <Button color="primary"
+                      variant="contained"
+                      style={{ marginLeft: '10px' }}
+                      onClick={() => this.getAllData()}
+                    >
+                      Draw Dropdown Chart
+        </Button>
+                  </>
+                }
+              />
+              <CardContent >
+                <GraphRecharts csvData={csvData} classes={classes} />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    );
+
+
+  }
 }
 function mapStateToProps(state) {
-    const { graphReducer={} } = state
-    return {
-      isLoading: graphReducer.isFileUploading,
-      csvData: graphReducer.csvData,
+  const { graphReducer = {} } = state
+  return {
+    isLoading: graphReducer.isFileUploading,
+    csvData: graphReducer.csvData,
 
-    }
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return bindActionCreators(
-        {
-            uploadCsv,
-            fetchAllData
-            // add other watcher sagas to this object to map them to props
-        },
-        dispatch
-    )
+  return bindActionCreators(
+    {
+      uploadCsv,
+      fetchAllData
+      // add other watcher sagas to this object to map them to props
+    },
+    dispatch
+  )
 }
 
-export default connect( mapStateToProps,
-    mapDispatchToProps)
- (withStyles(styles)(GraphPage));
+export default connect(mapStateToProps,
+  mapDispatchToProps)
+  (withStyles(styles)(GraphPage));
